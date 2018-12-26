11cece7b44d16d86a3a7f0704a3aa52ba8e37800
yaegaki <yaegaki.fino@gmail.com>
2018-12-27 01:01:00 +0900

    メッセージを集めてjsonとして出力するスクリプト追加

---
+++ src/extract-message-json.ts
@@ -0,0 +1,36 @@
+import { readdirSync, existsSync, readFileSync, mkdirSync, writeFileSync } from "fs";
+import { LiveChatLogInfo } from "./lib/dig-chat";
+import { parseChat } from "./lib/parse-chat";
+
+/**
+ * extract-messageのjson出力版
+ * json版は時間の情報も一緒に出力する
+ * ファイル名は${videoid}.json
+ */
+
+ const videos = readdirSync('./video');
+ videos.forEach(videoId => {
+     const infoPath = `video/${videoId}/chat/top/info.json`;
+     if (!existsSync(infoPath)) return;
+
+     if (!existsSync('message')) {
+         mkdirSync('message');
+     }
+     const outputPath = `message/${videoId}.json`
+
+     try
+     {
+        const info: LiveChatLogInfo = JSON.parse(readFileSync(infoPath, 'utf-8'))
+        const encoded = parseChat(info);
+        const messages = encoded.records.map(r => {
+            return { message: r.message, offsetMsec: r.offsetMsec };
+        });
+        writeFileSync(outputPath, JSON.stringify(messages));
+        console.log(`${videoId} done.`);
+     }
+     catch (e)
+     {
+         console.log(`${videoId} failed.`)
+         console.error(e);
+     }
+ });
