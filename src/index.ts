060eafc9abe9b73b2d612c897f9a356332d8010d
yaegaki <yaegaki.fino@gmail.com>
2019-01-18 21:56:25 +0900

    引数で指定された動画について取得するように変更

--- src/index.ts
+++ src/index.ts
@@ -4,33 +4,21 @@ import { getChromePath } from './lib/get-chromepath';
 import { parseChat } from './lib/parse-chat';
 import { digVideo } from './lib/dig-video';
 import { createPage } from './lib/pt-util';
+import * as process from 'process';
 
 (async () => {
-    const videoId = 'itCtbmck-vM';
+    if (process.argv.length < 3) {
+        console.log('must need video id.');
+        return;
+    }
 
+    const videoId = process.argv[2];
     const page = await createPage();
-    const vInfo = await digVideo(page, videoId);
-    console.log('dig done.');
-
-    const encoded = parseChat(vInfo.topChatInfo!);
-    encoded.records.forEach(r => {
-        let durationSec = r.offsetMsec / 1000;
-        let durationStr = '';
-        if (durationSec > 3600) {
-            const h = Math.floor(durationSec / 3600);
-            durationSec -= h * 3600;
-            durationStr = `${h}h`;
-        }
-        if (durationSec > 60) {
-            const m = Math.floor(durationSec / 60);
-            durationSec -= m * 60;
-            durationStr += `${m}m`;
-        }
-        if (durationSec > 0) {
-            durationSec = Math.floor(durationSec);
-            durationStr += `${durationSec}s`;
-        }
-
-        console.log(`${durationStr}:${r.message}`);
-    });
+    try {
+        await digVideo(page, videoId);
+    }
+    catch (e) {
+        console.error(e);
+    }
+    console.log('done');
 })();
